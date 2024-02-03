abbb7edac316045fa5a831e3907b750ef755cfed
ali-alaa10 <alialaa4545454545@gmail.com>
2024-02-03 14:56:57 +0200

    first task

--- JS/script.js
+++ JS/script.js
@@ -77,6 +77,9 @@
 // console.log('a' >= 'z')
 // console.log('z' >= 'a')
 
+let uname = "Ali";
 
+let first = uname.padStart(uname.length+5, "$");
+let end = uname.padEnd(uname.length+5, "$");
 
-
+console.log(first + end.replace(uname,""));
