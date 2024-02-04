6a2877c7f8fb7c9e4ff28d47f0ba9b20d8d3af60
ali-alaa10 <alialaa4545454545@gmail.com>
2024-02-04 03:15:20 +0200

    N

--- JS/script.js
+++ JS/script.js
@@ -135,4 +135,33 @@
 // ===================================================================================================
 
 
+// let p_name = "Mac Book";
+// let p_price = 1500;
+// let p_Availability = true;
+// let CanBeShipped = true;
+// let OrderStatus = 2;
 
+// let orderStatusMessage;
+// switch (OrderStatus) {
+//     case 0:
+//         orderStatusMessage = "Pending";
+//         break;
+//     case 1:
+//         orderStatusMessage = "On Way";
+//         break;
+//     case 2:
+//         orderStatusMessage = "Delivered";
+//         break;
+//     default:
+//         orderStatusMessage = "Not Started";
+// }
+
+// if (p_Availability && CanBeShipped) {
+//     console.log(`You Can Buy ${p_name}`);
+// } else {
+//     console.log("Sorry, You Can't Buy It Now");
+// }
+
+// console.log(`Order Status: ${orderStatusMessage}`);
+
+// ===================================================================================================
