d9fd9bd36c4dc31a9e699757f73f4be698c8826b
ali-alaa10 <alialaa4545454545@gmail.com>
2024-02-06 03:43:34 +0200

    The new tasks start from task6 to task9

--- JS/script.js
+++ JS/script.js
@@ -198,3 +198,28 @@
 // console.log(output);
 
 // ===================================================================================================
+
+// let needle = ["js"];
+// let haystack = ["php", "js", "python"];
+
+// if (haystack.includes(needle[0])) {
+//   console.log("Found");
+// } else {
+//   console.log("Not Found");
+// }
+
+// // Second Way
+
+// let foundItem = haystack.find((item) => item === needle[0]);
+// console.log(foundItem);
+
+// // Third Way
+
+// let chek = haystack.indexOf("js");
+// console.log(chek);
+
+// // Four Way
+
+// console.log(haystack.includes("js"));
+
+// ===================================================================================================
