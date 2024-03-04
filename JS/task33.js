90f82c907523db8af8c936ac82f10f1a79f91ef9
ali-alaa10 <alialaa4545454545@gmail.com>
2024-03-04 21:02:26 +0200

    task33

--- JS/task33.js
+++ JS/task33.js
@@ -2,59 +2,85 @@ let All = document.getElementById("all");
 let Done = document.getElementById("done");
 let Pending = document.getElementById("pending");
 
-let counter = 0;
+let input = document.getElementById("taskInput");
+let TaskUl = document.getElementById("tasks");
+
+All.innerHTML = Fetched().length;
+Done.innerHTML = Fetched().length - Fetched("Pending").length;
+Pending.innerHTML = Fetched().length - Fetched("done").length;
+let counter = fetchCount() || 0;
 
 function createTask() {
-  let input = document.getElementById("taskInput");
-  let TaskTxt = input.value;
-
-  if (TaskTxt.length === 0 || TaskTxt.length < 10) {
-    alert(`Please put your task and Must'n be less 10 char`);
-    return;
+  if (input.value !== "") {
+    counter++;
+    let Uni_key = `Task_${counter}`;
+    let value = input.value;
+    localStorage.setItem(Uni_key, value);
+    TaskUl.innerHTML += `<li> <span data-key="${Uni_key}">${value}</span> <button class="del">X</button> </li>`;
+  } else {
+    alert("Please Put Your Tasks :) ");
   }
-
-  counter++;
-
-  let Unikey = `Task_${counter}`;
-  localStorage.setItem(Unikey, TaskTxt);
-
-  FetchData();
-
   input.value = "";
+  input.focus();
 }
 
-function FetchData() {
-  let TaskUl = document.getElementById("tasks");
-  TaskUl.innerHTML = "";
+function Fetched() {
+  let Obj = Object.entries(localStorage);
+  let fet = Obj.filter((value) => {
+    let key = value[0];
+    let val = value[1];
+    return key.startsWith("Task_");
+  }).sort();
+  return fet;
+}
 
-  const tasks = Object.keys(localStorage)
-    .filter((key) => key.startsWith("Task_"))
-    .sort();
+Fetched();
 
-  tasks.forEach((Unikey) => {
-    let TaskTxt = localStorage.getItem(Unikey);
-    let TaskLi = document.createElement("li");
-    TaskLi.textContent = TaskTxt;
-
-    let Del = document.createElement("button");
-    Del.textContent = "Del";
-    Del.onclick = function () {
-      DelKey(Unikey);
-    };
-
-    TaskLi.appendChild(Del);
-    TaskUl.appendChild(TaskLi);
-
-    TaskLi.onclick = function () {
-      TaskLi.classList.toggle("done");
-      Unikey = localStorage.setItem(`Task_Done`, TaskTxt);
-    };
+function Display() {
+  let My_Task = Fetched();
+  My_Task.forEach((value) => {
+    let key = value[0];
+    let val = value[1];
+    // let class_done = key.includes("_done") ? "class=done" : "";
+    TaskUl.innerHTML += `<li> <span data-key="${key}">${val}</span> <button class="del">X</button> </li>`;
   });
 }
 
-function DelKey(Unikey) {
-  localStorage.removeItem(Unikey);
-  FetchData();
+Display();
+
+document.querySelectorAll(".del").forEach((del) => {
+  del.onclick = function () {
+    let TaskLi = this.parentNode;
+    let key = TaskLi.children[0].getAttribute("data-key");
+    localStorage.removeItem(key);
+    TaskLi.remove();
+  };
+});
+
+document.querySelectorAll("span[data-key]").forEach((sp) => {
+  sp.onclick = function (e) {
+    let old = e.target.getAttribute("data-key");
+    if (old.includes("_done")) {
+      let New = old.replace("_done", "");
+      e.target.classList.remove("done");
+      this.setAttribute("data-key", New);
+      localStorage.setItem(New, localStorage[old]);
+      localStorage.removeItem(old);
+    } else {
+      let New = old + "_done";
+      e.target.classList.add("done");
+      this.setAttribute("data-key", New);
+      localStorage.setItem(New, localStorage[old]);
+      localStorage.removeItem(old);
+    }
+  };
+});
+
+function fetchCount() {
+  let ArrLi = Fetched();
+  let LastLi = ArrLi[ArrLi.length - 1];
+  // let sliced = LastLi.slice(LastLi.slice("_") , LastLi.indexOf("_done"));
+  console.log(LastLi);
 }
 
-// localStorage.clear();
+fetchCount();
